13281a75c1545b776e33c437cc8eae6366764c2a
Utkatsh Kaule <utkarshkaule7@gmail.com>
2024-08-24 07:25:07 +0530

    connecting server

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
- const express = require('express')
-const products = require('./data/products')
- const app = express();
- app.get('/',(req,res)=>{
-    res.send("api is running")
- })
- app.get('/api/products',(req,res)=>{
-    res.json(products)
- })
- app.get('/api/products/:id',(req,res)=>{
-    const product = products.find(p=>p._id === req.params.id)
-    res.json(product)
- })
- app.listen(5000,console.log("app is running at 5000"))
+const express = require("express");
+const dotenv = require("dotenv");
+const products = require("./data/products");
+
+dotenv.config()
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
